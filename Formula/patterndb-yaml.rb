57dfa50385d7b7748906c56059ee15b70fb14e05
Jeffrey Urban <Jeffrey@JeffreyUrban.com>
2025-12-02 08:56:07 -0500

    fix: Address Formula audit issues
    - Add libyaml dependency for pyyaml resource
    - Update typer and rich resource URLs to use PyPI Source URLs
    - Resolves FormulaAudit/PyPiUrls and FormulaAudit/ResourceRequiresDependencies warnings

--- Formula/patterndb-yaml.rb
+++ Formula/patterndb-yaml.rb
@@ -8,17 +8,18 @@ class PatterndbYaml < Formula
   license "MIT"
 
   # Runtime dependencies
+  depends_on "libyaml"
   depends_on "python@3.12"
   depends_on "syslog-ng"
 
   # Python package dependencies
   resource "typer" do
-    url "https://files.pythonhosted.org/packages/source/t/typer/typer-0.15.1.tar.gz"
+    url "https://files.pythonhosted.org/packages/b8/e5/ab0e01be35ca2d5389c6c402eb54e10633aba159608d7270292b6f882a45/typer-0.15.1.tar.gz"
     sha256 "a0588c0a7fa68a1978a069818657778f86abe6ff5ea6abf472f940a08bfe4f0a"
   end
 
   resource "rich" do
-    url "https://files.pythonhosted.org/packages/source/r/rich/rich-13.9.4.tar.gz"
+    url "https://files.pythonhosted.org/packages/ab/3a/0316b28d0761c6734d6bc14e770d85506c986c85ffb239e688eeaab2c2bc/rich-13.9.4.tar.gz"
     sha256 "439594978a49a09530cff7ebc4b5c7103ef57baf48d5ea3184f21d9a2befa098"
   end
 
